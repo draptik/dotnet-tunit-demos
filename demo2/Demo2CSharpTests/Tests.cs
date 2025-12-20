ea31bb19ecb801572594829892939d9f021b1d24
Patrick Drechsler <patrick@pdrechsler.de>
2025-12-21 00:16:27 +0100

    ✨ demo2: workaround for NU1504 in F# projects

---
+++ demo2/Demo2CSharpTests/Tests.cs
@@ -0,0 +1,10 @@
+namespace Demo2CSharpTests;
+
+public class Tests
+{
+    [Test]
+    public void Basic()
+    {
+        Console.WriteLine("This is a basic test");
+    }
+}
---
+++ demo2/Demo2FSharpTests/Tests.fs
@@ -0,0 +1,16 @@
+﻿namespace Demo2FSahrpTests
+
+open System
+open TUnit.Core
+open TUnit.Assertions
+open TUnit.Assertions.Extensions
+open TUnit.Assertions.FSharp.Operations
+
+type Tests() =
+
+    [<Test>]
+    member _.Assert_Something() =
+        async {
+            let result = 1 + 2
+            do! check (Assert.That(result).IsEqualTo(3))
+        }
